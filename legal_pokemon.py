d1832e96b21a1e9bf3949370b5f47ec3879c7c15
Blank2208 <tomtom220801@gmail.com>
2024-12-06 11:11:17 +0100

    added die Regulation Marks

--- legal_pokemon.py
+++ legal_pokemon.py
@@ -1,18 +1,18 @@
 import requests
 import pandas as pd
 
-
+# Generelles
 # Einstellungen für Pandas, sonst ist das Terminal zu klein
 pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 
-
-
 url = "https://api.pokemontcg.io/v2/cards"  # API URL
 params = {"q": "set.legalities.standard:legal"}  # Query-Parameter um legale Pokemon zu kriegen, (in Dokumentation)
 # params werden genutzt, um der API bei der Anfrage einen genaueren Parameter mitzugeben. Dadurch kann man filtern.
 response = requests.get(url, params=params)
+
+
 
 # Überprüft, ob die Anfrage erfolgreich war
 if response.status_code == 200:
@@ -21,6 +21,8 @@ if response.status_code == 200:
     # 'data' enthält eine Struktur, die von der API vorgegeben ist
     cards = data.get("data", [])  # Holen der Karten (falls vorhanden)
 
+    for card in cards:
+        print(f'Name: {card["name"]}, Mark: {card["regulationMark"]}')
     df = pd.DataFrame(cards)    # Wandelt die gefetchten Daten in eine überschaubare Tabelle um
     print(df)
 
