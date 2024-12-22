edd66c67f260912268044df63f844add4cfbb6c7
Blank2208 <tomtom220801@gmail.com>
2024-12-22 18:26:47 +0100

    Überarbeitung der Assoziationsregeln

--- web scraping/Assoziationsregeln.py
+++ web scraping/Assoziationsregeln.py
@@ -2,19 +2,26 @@ import pandas as pd
 from mlxtend.frequent_patterns import apriori, association_rules
 from mlxtend.preprocessing import TransactionEncoder
 
-
 # Funktion, um die CSV-Datei zu laden und die Daten für den Apriori-Algorithmus vorzubereiten
 def prepare_data_for_apriori(file_path):
-    # Lade das Deck aus der CSV-Datei
+    # Gesamte Deckliste(n) werden geladen -> Alle Meta Decks in diesem Jahr (2024)
     df = pd.read_csv(file_path)
-    df_pokemon = df[df['Type'] == 'Pokemon']
-    # Die Karten aus der "Card Name"-Spalte extrahieren und pro Deck in Listen umwandeln
-    # transactions = df.groupby('Deck Name')['Card Name'].apply(list).tolist()
-    transactions = df_pokemon.groupby('Deck Name')['Card Name'].apply(list).tolist()
 
-    # Umwandeln der Transaktionen in das für Apriori erforderliche Format
+    # Filterungen
+    # df_pokemon = df[df['Type'] == 'Pokemon']
+    # df_trainer = df[df['Type'] == 'Trainer']
+
+    transactions_list = []
+
+    for deck_name, group in df.groupby('Deck Name'):
+        transaction = []
+        for _, row in group.iterrows():
+            transaction.extend([row['Card Name']])
+        transactions_list.append(transaction)
+
+    # Umwandeln der Transaktionen in Apriori Format
     te = TransactionEncoder()
-    te_ary = te.fit(transactions).transform(transactions)
+    te_ary = te.fit(transactions_list).transform(transactions_list)
     df_transformed = pd.DataFrame(te_ary, columns=te.columns_)
 
     return df_transformed
@@ -31,14 +38,13 @@ def generate_association_rules(file_path, min_support, min_threshold):
     frequent_itemsets = apriori(df_transformed, min_support=min_support, use_colnames=True)
     print(f"Frequente Itemsets gefunden: {len(frequent_itemsets)}")
 
-
     if frequent_itemsets.empty:
         print("Keine häufigen Itemsets gefunden.")
         return None
 
-
+    # Generierung der Assoziationsregeln
     print(f"Generierung der Assoziationsregeln...")
-    rules = association_rules(frequent_itemsets, metric="lift", min_threshold=min_threshold, num_itemsets=None)
+    rules = association_rules(frequent_itemsets, metric="lift", min_threshold=min_threshold, num_itemsets= None)
     print(f"Anzahl der generierten Regeln: {len(rules)}")
 
     # Wenn keine Regeln gefunden wurden, gebe eine Meldung aus
@@ -49,9 +55,9 @@ def generate_association_rules(file_path, min_support, min_threshold):
     return rules
 
 
-# Beispielaufruf: Assoziationsregeln für das Deck erstellen
-output_file = "decks.csv"  # Der Pfad zur CSV-Datei, die die Decks enthält
-rules = generate_association_rules(output_file, min_support=0.4, min_threshold=1)
+# Assoziationsaufruf
+output_file = "decklisten/decks_anpassung.csv"  # Decks, ohne die Spalte "Quantity", sondern untereinander geschrieben
+rules = generate_association_rules(output_file, min_support=0.15, min_threshold=1)
 
 # Ausgabe der generierten Assoziationsregeln
 if rules is not None:
@@ -59,6 +65,7 @@ if rules is not None:
     print(rules)
 
     # Speicherung der Assoziationsregeln in einer csv.-Datei
-    rules.to_csv("assoziationsregeln-low.csv", index=False)
+    rules.to_csv("assoziationsregeln-015.csv", index=False)
 else:
     print("Es wurden keine Assoziationsregeln generiert.")
+
